d7c5eb55fccaf6d06202ddf74d4972e71bab6675
Ronald Macario <ronaldmacario13@gmail.com>
2023-10-20 20:20:15 -0300

    refac: aplicando boas práticas no código do avião

--- Assets/Scripts/Aviao.cs
+++ Assets/Scripts/Aviao.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Aviao : MonoBehaviour {
 
-    Rigidbody2D fisica;
+    private Rigidbody2D fisica;
+    [SerializeField]
+    private float forca = 10;
 
     private void Awake() {
-        this.fisica = this.GetComponent<Rigidbody2D> ();
+        fisica = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     private void Update() {
         if(Input.GetButtonDown("Fire1")) {
             Debug.Log("Clicou");
-            this.Impulsionar();
+            Impulsionar();
         }
     }
 
     private void Impulsionar() {
-        this.fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        fisica.AddForce(Vector2.up * forca, ForceMode2D.Impulse);
     }
 }
