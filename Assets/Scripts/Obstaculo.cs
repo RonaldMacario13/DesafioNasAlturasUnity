12af503b218777017bc48e5282bbc71c6b978cab
Ronald Macario <ronaldmacario13@gmail.com>
2023-10-20 20:21:22 -0300

    refac: aplicando boas práticas no código do obstaculo

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstaculo : MonoBehaviour {
 
+    [SerializeField]
+    private float velocidade = 0.05f;
+
     // Update is called once per frame
     void Update() {
-        this.transform.Translate(Vector3.left * 0.05f);
+        transform.Translate(Vector3.left * velocidade);
     }
 }
