34e1b225687422401207b32d4daaac52328d7016
Ronald Macario <ronaldmacario13@gmail.com>
2023-10-21 19:21:56 -0300

    feat: adicionando efeito sonoro ao passar por obstáculo

--- Assets/Scripts/Pontuacao.cs
+++ Assets/Scripts/Pontuacao.cs
@@ -6,10 +6,16 @@ public class Pontuacao : MonoBehaviour {
     [SerializeField]
     private Text textoPontuacao;
     private int pontos;
+    private AudioSource audioPontuacao;
+
+    private void Awake() {
+        audioPontuacao = GetComponent<AudioSource>();
+    }
 
     public void AdicionarPontos() {
         pontos++;
         textoPontuacao.text = pontos.ToString();
+        audioPontuacao.Play();
     }
 
     public void ReiniciarPontuacao() {
