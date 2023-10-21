cba2531207f584d02445dc27d84a802282c1cbd5
Ronald Macario <ronaldmacario13@gmail.com>
2023-10-21 17:19:56 -0300

    feat: adicionando "Game Over" ao perder

--- Assets/Scripts/Diretor.cs
+++ Assets/Scripts/Diretor.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject imagemGameOver;
+
     public void FinalizarJogo() {
         Time.timeScale = 0;
+        imagemGameOver.SetActive(true);
     }
-
 }
