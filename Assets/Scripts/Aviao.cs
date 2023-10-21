99492a08d573550d6a2867818fd11e2ca569c45a
Ronald Macario <ronaldmacario13@gmail.com>
2023-10-21 12:08:15 -0300

    mntc: fazendo comentários importantes

--- Assets/Scripts/Aviao.cs
+++ Assets/Scripts/Aviao.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 public class Aviao : MonoBehaviour {
 
     private Rigidbody2D fisica;
-    [SerializeField]
-    private float forca = 10;
+    [SerializeField] // Isso faz com que seja mostrado no painel "Inspector" do Unity.
+    private float forca;
 
+    // Awake é chamado quando uma instância de script habilitada está sendo carregada.
     private void Awake() {
+        // Aqui estamos buscando o Rigidbidy2D que está no componete.
         fisica = GetComponent<Rigidbody2D> ();
     }
 
-    // Update is called once per frame
+    // Essa função é chamada a cada frame.
     private void Update() {
+        // Esse IF é responsável por chamar a função Impulsionar, que faz o avião "Voar".
         if(Input.GetButtonDown("Fire1")) {
-            Debug.Log("Clicou");
             Impulsionar();
         }
     }
 
+    // Função que faz o avião ser impulsinado para cima.
     private void Impulsionar() {
         fisica.AddForce(Vector2.up * forca, ForceMode2D.Impulse);
     }
