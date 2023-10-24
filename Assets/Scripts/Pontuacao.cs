f3e1deb8420d8c6c7aaf7af0281d7b1666e16733
Ronald Macario <ronaldmacario13@gmail.com>
2023-10-23 21:10:21 -0300

    mntc: adicionando cometários

--- Assets/Scripts/Pontuacao.cs
+++ Assets/Scripts/Pontuacao.cs
@@ -9,15 +9,18 @@ public class Pontuacao : MonoBehaviour {
     private AudioSource audioPontuacao;
 
     private void Awake() {
+        // Está sendo buscado o efeito sonoro realizado após passarmos por um obstáculo.
         audioPontuacao = GetComponent<AudioSource>();
     }
 
+    // Método responsável por adicionar pontos no contador da tela.
     public void AdicionarPontos() {
         pontos++;
         textoPontuacao.text = pontos.ToString();
         audioPontuacao.Play();
     }
 
+    // Método responsável por reiniciar a contagem de pontos da tela, após o fim de jogo.
     public void ReiniciarPontuacao() {
         pontos = 0;
         textoPontuacao.text = pontos.ToString();
