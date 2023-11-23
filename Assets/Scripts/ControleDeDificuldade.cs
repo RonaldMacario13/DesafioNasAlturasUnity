38cf69001b338c20af8dd3fe492f93f1cfaa549e
Ronald Macario <ronaldmacario13@gmail.com>
2023-11-23 10:59:46 -0300

    feat: adicionando controle de dificuldade

---
+++ Assets/Scripts/ControleDeDificuldade.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ControleDeDificuldade : MonoBehaviour {
+
+    [SerializeField]
+    private float dificuldadeMaxima;
+    private float dificuldade;
+    private float tempoPassado;
+
+    void Update() {
+
+    }
+}
